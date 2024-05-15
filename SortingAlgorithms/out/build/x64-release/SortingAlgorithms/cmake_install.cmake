4ddf1e4231251bbe13cee3f00b613fbfac076b3b
Dawid Brach <dbrach6@gmail.com>
2024-05-15 22:58:25 +0200

    Ignore

--- SortingAlgorithms/out/build/x64-release/SortingAlgorithms/cmake_install.cmake
+++ SortingAlgorithms/out/build/x64-release/SortingAlgorithms/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Dawid/Documents/GitHub/SortingAlgorithms/SortingAlgorithms/SortingAlgorithms
+# Install script for directory: C:/Users/dbrac/Documents/GitHub/SortingAlgorithms/SortingAlgorithms/SortingAlgorithms
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Dawid/Documents/GitHub/SortingAlgorithms/SortingAlgorithms/out/install/x64-release")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/dbrac/Documents/GitHub/SortingAlgorithms/SortingAlgorithms/out/install/x64-release")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
